6f1daf0da01fa6708bd33061bae0a27c0d6aa13f
David Butler <davidbutler@MacBook-Pro.cronet>
2020-05-10 22:15:35 +0000

    more buildroot / stash

--- build_root.bash
+++ build_root.bash
@@ -1,8 +1,12 @@
 echo "Not ready for general consumption, enter at your own risk"
 exit
 
-mkdir -p /buildroot
-cd build/root
+mkdir -p /build/root
+cd /build/root
 
 wget -O buildroot.tar.gz https://buildroot.org/downloads/buildroot-2020.05-rc1.tar.gz
 tar xvf buildroot.tar.gz
+
+cd /build/root/buildroot-2020.05-rc1/
+
+ln -sf /workspaces/the-bike-shed/buildroot.config .config
