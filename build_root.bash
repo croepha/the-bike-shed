9251fb230abaf600012abb099fc83e2275f01c22
David Butler <davidbutler@MacBook-Pro.cronet>
2020-05-15 04:25:21 +0000

    more buildroot

--- build_root.bash
+++ build_root.bash
@@ -1,8 +1,13 @@
 echo "Not ready for general consumption, enter at your own risk"
 exit
 
+apt install cpio rsync sudo
+useradd notroot
+
 mkdir -p /build/root
+chown -R notroot /build/root/
 cd /build/root
+sudo -u notroot bash
 
 wget -O buildroot.tar.gz https://buildroot.org/downloads/buildroot-2020.05-rc1.tar.gz
 
