331263da2c6f322a162b6b53602ed30a2ddedd2d
Dave Butler <croepha@gmail.com>
2020-05-17 06:29:12 +0000

    br

--- build_root.bash
+++ build_root.bash
@@ -1,7 +1,7 @@
 echo "Not ready for general consumption, enter at your own risk"
 exit
 
-apt install cpio rsync sudo ccache
+apt install -y cpio rsync sudo ccache
 useradd notroot
 
 mkdir -p /build/root
@@ -10,7 +10,10 @@ cd /build/root
 sudo -u notroot bash
 
 wget -O buildroot.tar.gz https://buildroot.org/downloads/buildroot-2020.05-rc1.tar.gz
-cd /build/root/buildroot-2020.05-rc1
+tar xvf buildroot.tar.gz
+mv /build/root/buildroot-2020.05-rc1/{.,}* /build/root/
+rmdir /build/root/buildroot-2020.05-rc1
+
 make clean
 make defconfig BR2_DEFCONFIG=/workspaces/the-bike-shed/root.config
 make
@@ -18,7 +21,13 @@ make
 
 
 sudo -u notroot bash
-cd /build/root/buildroot-2020.05-rc1
+cd /build/root
 make menuconfig
-make -C /build/root/buildroot-2020.05-rc1 savedefconfig BR2_DEFCONFIG=/workspaces/the-bike-shed/root.config
+make -C /build/root/buildroot-2020.05-rc1 savedefconfig \
+  BR2_DEFCONFIG=/workspaces/the-bike-shed/root.config
+
+make -C /build/root/buildroot-2020.05-rc1 savedefconfig
+
+
+
 
