e0926551fbe3416204789c49328a5f00051bddb9
Dave Butler <croepha@gmail.com>
2020-05-17 07:57:51 +0000

    br

--- build_root.bash
+++ build_root.bash
@@ -23,11 +23,18 @@ make
 sudo -u notroot bash
 cd /build/root
 make menuconfig
-make -C /build/root/buildroot-2020.05-rc1 savedefconfig \
-  BR2_DEFCONFIG=/workspaces/the-bike-shed/root.config
-
-make -C /build/root/buildroot-2020.05-rc1 savedefconfig
+make -C /build/root savedefconfig
 
 
+make linux-menuconfig
+make linux-update-defconfig -C /build/root BR2_LINUX_KERNEL_USE_CUSTOM_CONFIG=y BR2_LINUX_KERNEL_CUSTOM_CONFIG_FILE=/workspaces/the-bike-shed/linux.config
+
+make linux-savedefconfig -C /build/root BR2_LINUX_KERNEL_USE_CUSTOM_CONFIG=y BR2_LINUX_KERNEL_CUSTOM_CONFIG_FILE=/workspaces/the-bike-shed/linux.config
+
+make busybox-menuconfig
+make busybox-update-config -C /build/root BR2_PACKAGE_BUSYBOX_CONFIG=/workspaces/the-bike-shed/busybox.config
+
+make uclibc-menuconfig
+make uclibc-update-config -C /build/root BR2_UCLIBC_CONFIG=/workspaces/the-bike-shed/uclibc.config
 
 
