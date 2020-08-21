54a974fe60ebb479831ac7d558b2088e5fb1c752
Dave Butler <croepha@gmail.com>
2020-08-21 05:13:55 +0000

    .

--- io_core_test.bash
+++ io_core_test.bash
@@ -7,20 +7,25 @@ OUT_FILE=$3
 
 rm -f $OUT_FILE*
 
-(
+function fail() {
+    trap - ERR
+    echo "FAILED" >> "$OUT_FILE"
+    cat "$OUT_FILE"
+    exit -1
+}
+trap fail ERR
 
-  $EXEC 2>&1 | tee "$OUT_FILE.raw" |
-    sed -E $'s/^([0-9a-f]+[.][0-9]{3}: (DEBUG| INFO| WARN|ERROR|FATAL):.*)\t(.*)$/\\1\e[999C\e[50D\\3/' # >/dev/null # Right justify location info
+$EXEC 2>&1 | tee "$OUT_FILE.raw" |
+  sed -E $'s/^([0-9a-f]+[.][0-9]{3}: (DEBUG| INFO| WARN|ERROR|FATAL):.*)\t(.*)$/\\1\e[999C\e[50D\\3/' >/dev/null # Right justify location info
 
-  # remove timestamp, and also line number and addresses, if they shift around, we dont want the test to fail...
-  sed -E $'s/^[0-9a-f]+[.][0-9]{3}: ((DEBUG| INFO| WARN|ERROR|FATAL):.*\t)\((.*):.* .*:(.*)\)$/\\1(\\3 \\4)/' < "$OUT_FILE.raw" > "$OUT_FILE.cleaned1"
-  {
-    grep -v "test_sort:" < "$OUT_FILE.cleaned1"
-    sed -En 's/^(.*(test_sort:[^ ]*) .*)$/\2 \1/p' < "$OUT_FILE.cleaned1" | sort --stable --key=1,1 | sed -E 's/^(test_sort:[^ ]* )//'
-  } >  "$OUT_FILE.cleaned"
+# remove timestamp, and also line number and addresses, if they shift around, we dont want the test to fail...
+sed -E $'s/^[0-9a-f]+[.][0-9]{3}: ((DEBUG| INFO| WARN|ERROR|FATAL):.*\t)\((.*):.* .*:(.*)\)$/\\1(\\3 \\4)/' < "$OUT_FILE.raw" > "$OUT_FILE.cleaned1"
 
-  echo "to overwrite check: cp $OUT_FILE.cleaned $CHECK_FILE"
-  diff "$OUT_FILE.cleaned" "$CHECK_FILE"
-  echo PASSED
+grep -v "test_sort:" < "$OUT_FILE.cleaned1" >>  "$OUT_FILE.cleaned"
+sed -En 's/^(.*(test_sort:[^ ]*) .*)$/\2 \1/p' < "$OUT_FILE.cleaned1" |
+  sort --stable --key=1,1 | sed -E 's/^(test_sort:[^ ]* )//' >>  "$OUT_FILE.cleaned"
 
-) 2>&1 | tee "$OUT_FILE"
+
+echo "to overwrite check: cp $OUT_FILE.cleaned $CHECK_FILE" >> "$OUT_FILE"
+diff "$OUT_FILE.cleaned" "$CHECK_FILE" >> "$OUT_FILE"
+echo "PASSED" >> "$OUT_FILE"
