a8ebdce212174736db1e83a9f4cf0fbfd13d16c1
Dave Butler <croepha@gmail.com>
2020-08-23 13:16:24 +0000

    .

--- io_curl.c
+++ io_curl.c
@@ -1,5 +1,6 @@
+#include <curl/curl.h>
 #include <inttypes.h>
-
+#include <errno.h>
 #include "common.h"
 #include "logging.h"
 
@@ -24,7 +25,8 @@ static CURLSH * share;
 
 void io_curl_timeout() {
   int running_handles;
-  curl_multi_socket_action(multi, CURL_SOCKET_TIMEOUT, 0, &running_handles);
+  CURLMcode mr = curl_multi_socket_action(multi, CURL_SOCKET_TIMEOUT, 0, &running_handles);
+  error_check_curlm(mr);
 }
 
 void io_curl_io_event(struct epoll_event epe) {
@@ -37,7 +39,8 @@ void io_curl_io_event(struct epoll_event epe) {
     curl_ev |= CURL_CSELECT_OUT;
   int running_handles;
   io_EPData data = {.data = epe.data};
-  curl_multi_socket_action(multi, data.my_data.id, curl_ev, &running_handles);
+  CURLMcode mr = curl_multi_socket_action(multi, data.my_data.id, curl_ev, &running_handles);
+  error_check_curlm(mr);
 }
 
 static int socket_callback(CURL* easy, curl_socket_t fd, int action, void* u, void* s) {
@@ -45,25 +48,24 @@ static int socket_callback(CURL* easy, curl_socket_t fd, int action, void* u, vo
     io_EPData data = {.my_data = {.id = fd, .event_type = _io_socket_type_io_curl}};
     struct epoll_event epe = {.data = data.data};
 
-    switch (action) {
+    switch (action) { SWITCH_DEFAULT_IS_UNEXPECTED;
       case CURL_POLL_REMOVE: break;
       case CURL_POLL_IN : { epe.events=EPOLLIN ; } break;
       case CURL_POLL_OUT: { epe.events=EPOLLOUT; } break;
       case CURL_POLL_INOUT: { epe.events=EPOLLIN & EPOLLOUT; } break;
-      default: assert(0);
     };
 
     if (action == CURL_POLL_REMOVE) {
         int r1 = epoll_ctl(io_epoll_fd, EPOLL_CTL_DEL, fd, &epe);
-        assert(r1==0);
+        error_check(r1);
     } else if(!s) {
-      CURLMcode ret = curl_multi_assign(multi, fd, (void*)1);
-      assert(ret == CURLM_OK);
+      CURLMcode mr = curl_multi_assign(multi, fd, (void*)1);
+      error_check_curlm(mr);
       int r1 = epoll_ctl(io_epoll_fd, EPOLL_CTL_ADD, fd, &epe);
-      assert(r1==0);
+      error_check(r1);
     } else {
       int r1 = epoll_ctl(io_epoll_fd, EPOLL_CTL_MOD, fd, &epe);
-      assert(r1==0);
+      error_check(r1);
     }
     return 0;
 }
@@ -71,36 +73,40 @@ static int socket_callback(CURL* easy, curl_socket_t fd, int action, void* u, vo
 u8 io_curl_completed(CURL**easy, CURLcode*result, void*private) {
   CURLMsg *curl_msg;
   int curl_msg_left = 0;
-  if (*easy) {
-    curl_multi_remove_handle(multi, *easy);
-  }
   while (( curl_msg = curl_multi_info_read(multi, &curl_msg_left) )) {
     if (curl_msg->msg == CURLMSG_DONE) {
       *result = curl_msg->data.result;
       *easy = curl_msg->easy_handle;
+      CURLMcode mr = curl_multi_remove_handle(multi, *easy);
+      error_check_curlm(mr);
       CURLcode cr = curl_easy_getinfo(*easy, CURLINFO_PRIVATE, private);
       error_check_curl(cr);
       return 1;
     } else {
-      DEBUG("unknown\n");
+      ERROR("unknown\n");
     }
   }
   return 0;
 }
 
-void io_curl_initialize() {
+void io_curl_initialize() { CURLMcode mr; CURLcode cr; CURLSHcode sr;
   curl_version_info_data data = *curl_version_info(CURLVERSION_NOW);
   assert(data.features & CURL_VERSION_ASYNCHDNS);
 
-  curl_global_init(CURL_GLOBAL_DEFAULT);
+  cr = curl_global_init(CURL_GLOBAL_DEFAULT);
+  error_check_curl(cr);
 
   multi = curl_multi_init();
-  curl_multi_setopt(multi, CURLMOPT_SOCKETFUNCTION, socket_callback);
-  curl_multi_setopt(multi, CURLMOPT_TIMERFUNCTION , timer_callback);
+  mr = curl_multi_setopt(multi, CURLMOPT_SOCKETFUNCTION, socket_callback);
+  error_check_curlm(mr);
+  mr = curl_multi_setopt(multi, CURLMOPT_TIMERFUNCTION , timer_callback);
+  error_check_curlm(mr);
 
   share = curl_share_init();
-  curl_share_setopt(share, CURLSHOPT_SHARE, CURL_LOCK_DATA_SSL_SESSION);
-  curl_share_setopt(share, CURLSHOPT_SHARE, CURL_LOCK_DATA_COOKIE);
+  sr = curl_share_setopt(share, CURLSHOPT_SHARE, CURL_LOCK_DATA_SSL_SESSION);
+  error_check_curlsh(sr);
+  sr = curl_share_setopt(share, CURLSHOPT_SHARE, CURL_LOCK_DATA_COOKIE);
+  error_check_curlsh(sr);
 }
 
 //  curl_share_cleanup(share);
@@ -109,8 +115,10 @@ void io_curl_initialize() {
 CURL* io_curl_create_handle() {
   CURL* ret = curl_easy_init();
   assert(ret);
-  curl_multi_add_handle(multi, ret);
-  curl_easy_setopt(ret, CURLOPT_SHARE, share);
+  CURLMcode mr = curl_multi_add_handle(multi, ret);
+  error_check_curlm(mr);
+  CURLcode cr = curl_easy_setopt(ret, CURLOPT_SHARE, share);
+  error_check_curl(cr);
   return ret;
 }
 
--- io_curl.h
+++ io_curl.h
@@ -18,7 +18,27 @@
     curl_easy_strerror(_m_curl_code)); } \
 })
 
-#define error_check_curl(err) ({ if ((err)!= CURLE_OK) { ERROR("Curl error:%d:%s", err, curl_easy_strerror(err)); } })
+__attribute__((unused))
+static char const * _error_check_CURLcode(CURLcode c) {
+  if (c == CURLE_OK) { return 0; }
+  return curl_easy_strerror(c);
+}
+
+__attribute__((unused))
+static char const * _error_check_CURLMcode(CURLMcode c) {
+  if (c == CURLM_OK) { return 0; }
+  return curl_multi_strerror(c);
+}
+
+__attribute__((unused))
+static char const * _error_check_CURLSHcode(CURLSHcode c) {
+  if (c == CURLSHE_OK) { return 0; }
+  return curl_share_strerror(c);
+}
+
+#define error_check_curl(  err) ({ char const * error_check_s = _error_check_CURLcode  (err); if (error_check_s) { ERROR("Error   CURLcode:%d:%s", err, error_check_s); } })
+#define error_check_curlm( err) ({ char const * error_check_s = _error_check_CURLMcode (err); if (error_check_s) { ERROR("Error  CURLMcode:%d:%s", err, error_check_s); } })
+#define error_check_curlsh(err) ({ char const * error_check_s = _error_check_CURLSHcode(err); if (error_check_s) { ERROR("Error CURLSHcode:%d:%s", err, error_check_s); } })
 
 u8 io_curl_completed(CURL**easy, CURLcode*result, void*private);
 CURL* io_curl_create_handle();
