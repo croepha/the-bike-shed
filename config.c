6061a9b72c77d2477b4376a102bc396ddfca4273
Dave Butler <croepha@gmail.com>
2020-09-20 23:49:53 +0000

    .

--- config.c
+++ config.c
@@ -88,6 +88,60 @@ static void __config_append(struct StringList *sl, char* str) {
 }
 
 #if CONFIG_DIAGNOSTICS == 1
+char *   valid_config_email_from[] = {
+    "EmailAddress:    asdasdfasdasd32323@gmail.com",
+    "EmailAddress:    yahooyahoo@yahoo.com",
+    "EmailAddress: tmp-from@testtest.test",
+0};
+char * invalid_config_email_from[] = {
+    "EmailAddress:    asdasd@fasdasd32323@mail.com",
+    "EmailAddress:    yahooyahoo@",
+    "EmailAddress: @testtest.test",
+    "EmailAddress: @",
+    "EmailAddress: dasdfasdfasd",
+0};
+char *   valid_config_email_host[] = {
+    "EmailServer:  smtps://smtp.gmail.com",
+    "EmailServer:  smtps://smtp.gmail.com:465",
+    "EmailServer:  smtp://127.0.0.1",
+    "EmailServer:  smtp://127.0.0.1:8025",
+    "EmailServer:  smtps://127.0.0.1:8025",
+0};
+char * invalid_config_email_host[] = {
+    "EmailServer:  smtp.gmail.com",
+    "EmailServer:  smtp://127.0.0.1:",
+    "EmailServer:  smtpss://127.0.0.1:",
+    "EmailServer:  smtpss://:333",
+    "EmailServer:  smtps://127.0.0.1:8ddd025",
+0};
+char *   valid_config_email_rcpt[] = {
+    "DestinationEmailAddress:    asdasdfasdasd32323@gmail.com",
+    "DestinationEmailAddress:    yahooyahoo@yahoo.com",
+    "DestinationEmailAddress: tmp-from@testtest.test",
+0};
+char * invalid_config_email_rcpt[] = {
+    "DestinationEmailAddress:    asdasd@fasdasd32323@mail.com",
+    "DestinationEmailAddress:    yahooyahoo@",
+    "DestinationEmailAddress: @testtest.test",
+    "DestinationEmailAddress: @",
+    "DestinationEmailAddress: dasdfasdfasd",
+0};
+char *   valid_config_argv_config[] = {
+    "DebugSupervisorArg: /bin/sh",
+    "DebugSupervisorArg: -c",
+    "DebugSupervisorArg: /usr/bin/ping 127.0.0.1 | ts",
+0};
+char *   valid_config_email_user_pass[] = {
+    "EmailUserPass:  user:pass",
+    "EmailUserPass:  314234123klj;k;asdfasd!~!@:@!#@#$%@#&^&!~8709870asdfaklj",
+    "EmailUserPass:  :",
+0};
+char * invalid_config_email_user_pass[] = {
+    "EmailUserPass: ",
+    "EmailUserPass: adfasdfasdf",
+    "EmailUserPass: 12341231234",
+0};
+
 #define do_diagnostic(long_string, short_var) *end=0; \
   __do_diagnostic(long_string, start, valid_config_ ## short_var, line_number); return;
 static void __do_diagnostic(char * long_string, char * start, char** valid_examples, int line_number) {
--- config_diag_test.c
+++ config_diag_test.c
@@ -18,15 +18,15 @@ struct StringList tmp_arg;
     // for (char * c=buf; *c; c++) { *c=tolower(*c); }
 
 
-#define test_set(set, var) INFO("Testing set: %s", #set); { LOGCTX("\t"); _test_set( set, COUNT(set), &var); }
-static void _test_set(char**set, usz set_len, char** var) {
-    for (int i=0; i < set_len; i++) {
-        if (!set[i]) break;
+#define test_set(set, var) INFO("Testing set: %s", #set); { LOGCTX("\t"); _test_set( set, &var); }
+static void _test_set(char**set, char** var) {
+    for (;*set; set++) {
+        if (!*set) break;
         config_initialize();
         *var = 0;
-        INFO("Trying line: '%s'", set[i]);
+        INFO("Trying line: '%s'", *set);
         char buf[1024];
-        strcpy(buf, set[i]);
+        strcpy(buf, *set);
         log_allowed_fails = 100;
         config_parse_line(buf, 0);
         INFO("Effective: '%s' Failures: %d", *var, 100 - log_allowed_fails);
@@ -34,15 +34,15 @@ static void _test_set(char**set, usz set_len, char** var) {
     }
 }
 
-#define test_set2(set) INFO("Testing set: %s", #set); { LOGCTX("\t"); _test_set2( set, COUNT(set)); }
-static void _test_set2(char**set, usz set_len) {
+#define test_set2(set) INFO("Testing set: %s", #set); { LOGCTX("\t"); _test_set2( set); }
+static void _test_set2(char**set) {
     config_initialize();
     string_list_initialize(&tmp_arg);
-    for (int i=0; i < set_len; i++) {
-        if (!set[i]) break;
-        INFO("Trying line: '%s'", set[i]);
+    for (;*set; set++) {
+        if (!*set) break;
+        INFO("Trying line: '%s'", *set);
         char buf[1024];
-        strcpy(buf, set[i]);
+        strcpy(buf, *set);
         log_allowed_fails = 100;
         config_parse_line(buf, 0);
         INFO("Failures: %d", 100 - log_allowed_fails);
@@ -52,61 +52,6 @@ static void _test_set2(char**set, usz set_len) {
         INFO("Effective: %s",sll->str);
     }
 }
-char *   valid_config_email_from[] = {
-    "EmailAddress:    asdasdfasdasd32323@gmail.com",
-    "EmailAddress:    yahooyahoo@yahoo.com",
-    "EmailAddress: tmp-from@testtest.test",
-0};
-char * invalid_config_email_from[] = {
-    "EmailAddress:    asdasd@fasdasd32323@mail.com",
-    "EmailAddress:    yahooyahoo@",
-    "EmailAddress: @testtest.test",
-    "EmailAddress: @",
-    "EmailAddress: dasdfasdfasd",
-0};
-char *   valid_config_email_host[] = {
-    "EmailServer:  smtps://smtp.gmail.com",
-    "EmailServer:  smtps://smtp.gmail.com:465",
-    "EmailServer:  smtp://127.0.0.1",
-    "EmailServer:  smtp://127.0.0.1:8025",
-    "EmailServer:  smtps://127.0.0.1:8025",
-0};
-char * invalid_config_email_host[] = {
-    "EmailServer:  smtp.gmail.com",
-    "EmailServer:  smtp://127.0.0.1:",
-    "EmailServer:  smtpss://127.0.0.1:",
-    "EmailServer:  smtpss://:333",
-    "EmailServer:  smtps://127.0.0.1:8ddd025",
-0};
-char *   valid_config_email_rcpt[] = {
-    "DestinationEmailAddress:    asdasdfasdasd32323@gmail.com",
-    "DestinationEmailAddress:    yahooyahoo@yahoo.com",
-    "DestinationEmailAddress: tmp-from@testtest.test",
-0};
-char * invalid_config_email_rcpt[] = {
-    "DestinationEmailAddress:    asdasd@fasdasd32323@mail.com",
-    "DestinationEmailAddress:    yahooyahoo@",
-    "DestinationEmailAddress: @testtest.test",
-    "DestinationEmailAddress: @",
-    "DestinationEmailAddress: dasdfasdfasd",
-0};
-char *   valid_config_argv_config[] = {
-    "DebugSupervisorArg: /bin/sh",
-    "DebugSupervisorArg: -c",
-    "DebugSupervisorArg: /usr/bin/ping 127.0.0.1 | ts",
-0};
-char *   valid_config_email_user_pass[] = {
-    "EmailUserPass:  user:pass",
-    "EmailUserPass:  314234123klj;k;asdfasd!~!@:@!#@#$%@#&^&!~8709870asdfaklj",
-    "EmailUserPass:  :",
-0};
-char * invalid_config_email_user_pass[] = {
-    "EmailUserPass: ",
-    "EmailUserPass: adfasdfasdf",
-    "EmailUserPass: 12341231234",
-0};
-
-
 
 
 int main () {
