5398610255ce25f6f2198a132515881ce86abd34
David Butler <davidbutler@MacBook-Pro.cronet>
2020-09-23 07:11:16 +0000

    .

--- access_test.c
+++ access_test.c
@@ -3,7 +3,7 @@
 
 #include "logging.h"
 #include "common.h"
-// #include "access.h"
+#include "access.h"
 
 static usz const SALT_BUF_LEN = 64;
 static usz const RFID_BUF_LEN = 24;
@@ -15,18 +15,11 @@ u8 access_requested(char * rfid, char * pin);
 
 typedef u8 access_HashResult[64];
 
-void access_hash(access_HashResult * result, void* data, usz data_len);
 
-void access_hash(access_HashResult * result, void* data, usz data_len) {
-    INFO_HEXBUFFER(data, data_len, "data:");
+void access_hash(access_HashResult * result, struct access_HashPayload * payload) {
+    INFO_HEXBUFFER(payload, sizeof * payload, "payload:");
     memset(result, 0x69, sizeof * result);
 }
-
-struct access_HashPayload {
-  char salt[SALT_BUF_LEN];
-  char rfid[RFID_BUF_LEN];
-  char pin[PIN_BUF_LEN];
-};
 
 u8 access_requested(char * rfid, char * pin) {
   struct access_HashPayload payload = {};
@@ -34,7 +27,7 @@ u8 access_requested(char * rfid, char * pin) {
   memcpy(payload.rfid, rfid, RFID_BUF_LEN);
   memcpy(payload.pin , pin , PIN_BUF_LEN );
   access_HashResult hash_result;
-  access_hash(&hash_result, &payload, sizeof payload);
+  access_hash(&hash_result, &payload);
   INFO_HEXBUFFER(&hash_result, sizeof hash_result, "hash_result:");
   return 0;
 }
