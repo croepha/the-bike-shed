d5d94b4ab3c67153938497001af68957465af3e2
Dave Butler <croepha@gmail.com>
2020-09-26 23:02:53 +0000

    .

--- access_test.c
+++ access_test.c
@@ -1,6 +1,8 @@
 
 #include <string.h>
 
+#include <time.h>
+
 #include "logging.h"
 #include "common.h"
 #include "access.h"
@@ -39,69 +41,144 @@ void access_user_list_init(void);
 u16 access_users_first_free_idx;
 u16 access_users_first_idx;
 
+#define USER (access_users_space[USER_idx])
+#define MAP (access_map[MAP_idx])
+
+
 void access_user_list_init(void) {
 
   // Initialize the free list
   access_users_first_free_idx = -1;
-  for (u16 i = 0; i < USER_TABLE_LEN; i++) {
-    access_users_space[i].debug_is_free = 1;
-    access_users_space[i].next_idx = access_users_first_free_idx;
-    access_users_first_free_idx = i;
+  for (u16 USER_idx = 0; USER_idx < USER_TABLE_LEN; USER_idx++) {
+
+    USER.debug_is_free = 1;
+    USER.next_idx = access_users_first_free_idx;
+    access_users_first_free_idx = USER_idx;
+
   }
 }
 
 static u32 get_hash_mask(u32 v) { return v & (HASH_MAP_LEN - 1); }
 static u32 get_hash_i(access_HashResult hash_result) { return get_hash_mask(*(u32*)hash_result); }
 
-// static void access_user_remove_i(u32 hash_i) {
-
+// // TODO
+// access_user_remove_i
+// void access_user_remove_i(u32 MAP_idx) {
 // }
+void access_idle_maintenance(void);
+void access_idle_maintenance(void) {
 
-#define USER (access_users_space[idx])
+  // Scan for users that need to be pruned
+
+  u16 * prev = &access_users_first_idx;
+
+  if (*prev == (u16)-1) {
+    return;
+  }
+
+  u16 USER_idx = *prev;
+
+
+  assert(!USER.debug_is_free);
+
+  u64  utc_sec = time(0);
+  u64  pt_day = (utc_sec - 60*60*7 /* 7 hours */ ) / 60*60*24;
+
+  u32 map_first_tombstone = (u32)-1;
+
+  u32 map_rage_start_idx = get_hash_i(USER.hash);
+  u32 map_range_end = map_rage_start_idx;
+
+  for (u32 MAP_idx = map_rage_start_idx;; MAP_idx = get_hash_mask(MAP_idx + 1) ) {
+    if (MAP == (u16)-1 && map_first_tombstone == (u32)-1) {
+      map_first_tombstone = MAP_idx;
+    } else if (MAP == 0) {
+      assert(0);
+      map_range_end = MAP_idx;
+      break; // Hash not in our map
+    } else {
+      map_range_end = MAP_idx;
+      assert(USER_idx == MAP - 1);
+      if (USER.expire_day < pt_day) { // This entry is expired lets remove it
+        USER.debug_is_free = 1;
+        USER.next_idx = access_users_first_free_idx;
+        access_users_first_free_idx = USER_idx;
+        MAP = -1;
+        *prev = USER.next_idx;
+      } else {
+        if (map_first_tombstone != (u32)-1) {
+          // We hit a tombstone on the way, lets go ahead and swap this entry with that one
+          MAP = (u16)-1;
+          MAP_idx = map_first_tombstone;
+          MAP = USER_idx + 1;
+        }
+        prev = &USER.next_idx;
+      }
+      break;
+    }
+  }
+
+  // Walk the map range backwards, looking for tombstones
+}
+
 void access_user_add(access_HashResult hash, u16 expire_day) {
 
   if (access_users_first_free_idx == (u16)-1) {
     ERROR("User list full");
-    return;
+    // Try real hard to fee up some space
+    access_idle_maintenance();
+    if (access_users_first_free_idx == (u16)-1) {
+      INFO("No space could be freed, giving up");
+      return;
+    }
   }
 
-  u32 hash_i = get_hash_i(hash);
-  for (;;hash_i = get_hash_mask(hash_i + 1) ) {
-    if (access_map[hash_i] != 0 && access_map[hash_i] != (u16)-1) {
-      u16 idx = access_map[hash_i] + 1;
+  u32 map_first_tombstone = (u32)-1;
+  for (u32 MAP_idx = get_hash_i(hash);; MAP_idx = get_hash_mask(MAP_idx + 1) ) {
+    if (MAP == (u16)-1 && map_first_tombstone == (u32)-1) {
+      map_first_tombstone = MAP_idx;
+    } else if (MAP == 0) {
+      // Found empty space, make new:
+      if (map_first_tombstone != (u32)-1) {
+        // If we had hit a tombstone, lets fill that slot instead
+        MAP_idx = map_first_tombstone;
+      }
+
+      // pop off the free list
+      u16 USER_idx = access_users_first_free_idx;
+      access_users_first_free_idx = USER.next_idx;
+      assert(USER.debug_is_free);
+
+
+      USER.debug_is_free = 0;
+      USER.next_idx = access_users_first_idx;
+      access_users_first_idx = USER_idx;
+
+      MAP = USER_idx + 1;
+      memcpy(USER.hash, hash, sizeof USER.hash);
+
+      USER.expire_day = expire_day;
+      break;
+    } else {
+      u16 USER_idx = MAP - 1;
       if (memcmp(USER.hash, hash, sizeof USER.hash) == 0) {
         // Update existing
         USER.expire_day = expire_day;
-
-      }
-    } else {  // Found empty space make new:
-      // pop off the free list
-      u16 idx = access_users_first_free_idx;
-      access_users_first_free_idx = USER.next_idx;
-      assert(USER.debug_is_free);
-      USER.debug_is_free = 0;
-
-      USER.next_idx = access_users_first_idx;
-      access_users_first_idx = idx;
-
-      memcpy(USER.hash, hash, sizeof USER.hash);
-      USER.expire_day = expire_day;
-
+        break;
+      } // else continue
     }
   }
 
 }
 
-
-#include <time.h>
 u8 access_requested(char * rfid, char * pin) {
   struct access_HashPayload payload = {};
   memcpy(payload.salt, salt, sizeof payload.salt);
   memcpy(payload.rfid, rfid, sizeof payload.rfid);
   memcpy(payload.pin , pin , sizeof payload.pin );
-  access_HashResult hash_result;
-  access_hash(&hash_result, &payload);
-  INFO_HEXBUFFER(&hash_result, sizeof hash_result, "hash_result:");
+  access_HashResult hash;
+  access_hash(&hash, &payload);
+  INFO_HEXBUFFER(&hash, sizeof hash, "hash_result:");
 
   assert( !get_hash_mask(HASH_MAP_LEN) ); // Assert HASH_MAP_SIZE is power of 2
   assert( HASH_MAP_LEN >= USER_TABLE_LEN);
@@ -111,22 +188,31 @@ u8 access_requested(char * rfid, char * pin) {
   u64  utc_sec = time(0);
   u64  pt_day = (utc_sec - 60*60*7 /* 7 hours */ ) / 60*60*24;
 
-  u32 hash_i = get_hash_i(hash_result);
-  for (;;hash_i = get_hash_mask(hash_i + 1) ) {
-    if (access_map[hash_i] != 0 && access_map[hash_i] != (u16)-1) {
-      struct accessUser * user = &access_users_space[access_map[hash_i] + 1];
-      if (memcmp(user->hash, hash_result, sizeof user->hash) == 0) {
-        // Found
-        if (user->expire_day <= pt_day) {
+  u32 map_first_tombstone = (u32)-1;
+  for (u32 MAP_idx = get_hash_i(hash);; MAP_idx = get_hash_mask(MAP_idx + 1) ) {
+    if (MAP == (u16)-1 && map_first_tombstone == (u32)-1) {
+      map_first_tombstone = MAP_idx;
+    } else if (MAP == 0) {
+      return 0; // Hash not in our map
+    } else {
+      u16 USER_idx = MAP - 1;
+      if (memcmp(USER.hash, hash, sizeof USER.hash) == 0) { // Found
+        if (map_first_tombstone != (u32)-1) {
+          // We hit a tombstone on the way, lets go ahead and swap this entry with that one
+          MAP = (u16)-1;
+          MAP_idx = map_first_tombstone;
+          MAP = USER_idx + 1;
+        }
+
+        if (USER.expire_day <= pt_day) {
           return 1; // Valid
         } else {
           return 0; // Expired
         }
       } // else continue
-    } else {
-      return 0; // Hash not in our map
     }
   }
+  __builtin_unreachable();
   return 0; // unreachable
 }
 
