af6e74b8f058ab27d694de589ae707b919a6d75d
Dave Butler <croepha@gmail.com>
2020-11-13 02:42:10 +0000

    .

--- access_data.c
+++ access_data.c
@@ -128,83 +128,6 @@ enum {
 //   }
 // }
 
-void access_idle_maintenance(void) {
-
-  // Scan for users that need to be pruned
-  if (*access_idle_maintenance_prev == access_user_NOT_FOUND) {
-    return;
-  }
-
-  access_user_IDX USER_idx = *access_idle_maintenance_prev;
-  //DEBUG("USER_idx:%hu", USER_idx);
-  assert(!USER.debug_is_free);
-
-  // LOGCTX(" USER_idx:%x ", USER_idx );
-  // // Lets remove people if they have been expired for at-least 60 days
-  // u8 is_expired = access_user_days_left(USER_idx) <  -60;
-  // if (is_expired) { // This entry is expired lets remove it
-  //   TRACE("Expired, freeing");
-  //   USER.debug_is_free = 1;
-  //   *access_idle_maintenance_prev = USER.next_idx;
-  //   USER.next_idx = access_users_first_free_idx;
-  //   access_users_first_free_idx = USER_idx;
-  // }
-
-  u32 map_first_tombstone = (u32)-1;
-
-  u32 map_rage_start_idx = get_hash_i(USER.hash);
-  u32 map_range_end = map_rage_start_idx;
-
-  for (u32 MAP_idx = map_rage_start_idx;; MAP_idx = get_hash_mask(MAP_idx + 1) ) {
-    if (MAP == map_TOMB) {
-      if (map_first_tombstone == (u32)-1) {
-        TRACE("MAP_idx:%x Hit first tombstone ", MAP_idx);
-        map_first_tombstone = MAP_idx;
-      }
-    } else if (MAP == map_EMPTY) {
-      TRACE("MAP_idx:%x Hash not in our map ", MAP_idx);
-      assert(0);
-      map_range_end = MAP_idx;
-      break; // Hash not in our map
-    } else {
-      map_range_end = MAP_idx;
-      if(USER_idx == MAP - 1) {
-        u8 is_expired;
-        {
-          LOGCTX(" MAP_idx:%x USER_idx:%x ", MAP_idx, USER_idx );
-          // Lets remove people if they have been expired for at-least 60 days
-          is_expired = access_user_days_left(USER_idx) <  -60;
-        }
-        if (is_expired) { // This entry is expired lets remove it
-          TRACE("MAP_idx:%x USER_idx:%x Expired, freeing", USER_idx, MAP_idx);
-          USER.debug_is_free = 1;
-          *access_idle_maintenance_prev = USER.next_idx;
-          USER.next_idx = access_users_first_free_idx;
-          access_users_first_free_idx = USER_idx;
-          MAP = (u16)-1;
-        } else {
-          if (map_first_tombstone != (u32)-1) {
-            TRACE("MAP_idx:%x USER_idx:%x We hit a tombstone on the way, lets go ahead and swap this entry(%x) with that one ", map_first_tombstone, USER_idx, MAP_idx);
-            MAP = (u16)map_TOMB;
-            MAP_idx = map_first_tombstone;
-            MAP = USER_idx + 1;
-          }
-          TRACE("MAP_idx:%x USER_idx:%x Not expired, doing nothing", USER_idx, MAP_idx);
-          access_idle_maintenance_prev = &USER.next_idx;
-        }
-        break;
-      } else {
-        // TODO, should this really be a warning? or at-least an INFO?
-        TRACE("MAP_idx:%x USER_idx:%x Collision, continuing", MAP_idx, USER_idx);
-      }
-    }
-  }
-
-  // TODO: We could Walk the map range backwards, looking for tombstones... probably not worth it though
-
-}
-
-
 
 static u32 __user_map_lookup(access_HashResult hash) {
   TRACE_HEXBUFFER(hash, 64 / 8, "hash:");
@@ -245,6 +168,126 @@ static u32 __user_map_lookup(access_HashResult hash) {
   ERROR("got to unreachable code");
   return -1; // unreachable
 }
+
+
+void access_idle_maintenance(void) {
+
+  // Scan for users that need to be pruned
+  if (*access_idle_maintenance_prev == access_user_NOT_FOUND) {
+    return;
+  }
+
+  access_user_IDX USER_idx = *access_idle_maintenance_prev;
+  //DEBUG("USER_idx:%hu", USER_idx);
+  assert(!USER.debug_is_free);
+
+  LOGCTX(" USER_idx:%x ", USER_idx );
+
+#if 0
+  u32 MAP_idx = __user_map_lookup(USER.hash);
+  LOGCTX(" MAP_idx:%x ", MAP_idx );
+  LOGCTX(" USER_idx:%x ", USER_idx );
+
+  // Lets remove people if they have been expired for at-least 60 days
+  u8 is_expired = access_user_days_left(USER_idx) <  -60;
+
+  if (is_expired) { // This entry is expired lets remove it
+
+
+    if (MAP_idx == (u32)-1) {
+      access_idle_maintenance_prev = &USER.next_idx;
+      ERROR();
+      return;
+    }
+    if (MAP == map_EMPTY || MAP == map_TOMB) {
+      ERROR();
+      return;
+    }
+
+    TRACE("Expired, freeing");
+    USER.debug_is_free = 1;
+    *access_idle_maintenance_prev = USER.next_idx;
+    USER.next_idx = access_users_first_free_idx;
+    access_users_first_free_idx = USER_idx;
+    MAP = (u16)-1;
+
+  } else {
+    TRACE("Not expired, doing nothing");
+    access_idle_maintenance_prev = &USER.next_idx;
+  }
+#endif
+
+
+
+
+#if 1
+
+  // LOGCTX(" USER_idx:%x ", USER_idx );
+  // // Lets remove people if they have been expired for at-least 60 days
+  // u8 is_expired = access_user_days_left(USER_idx) <  -60;
+  // if (is_expired) { // This entry is expired lets remove it
+  //   TRACE("Expired, freeing");
+  //   USER.debug_is_free = 1;
+  //   *access_idle_maintenance_prev = USER.next_idx;
+  //   USER.next_idx = access_users_first_free_idx;
+  //   access_users_first_free_idx = USER_idx;
+  // }
+
+  u32 map_first_tombstone = (u32)-1;
+
+  u32 map_rage_start_idx = get_hash_i(USER.hash);
+  u32 map_range_end = map_rage_start_idx;
+
+  for (u32 MAP_idx = map_rage_start_idx;; MAP_idx = get_hash_mask(MAP_idx + 1) ) {
+    if (MAP == map_TOMB) {
+      if (map_first_tombstone == (u32)-1) {
+        TRACE("MAP_idx:%x Hit first tombstone ", MAP_idx);
+        map_first_tombstone = MAP_idx;
+      }
+    } else if (MAP == map_EMPTY) {
+      TRACE("MAP_idx:%x Hash not in our map ", MAP_idx);
+      assert(0);
+      map_range_end = MAP_idx;
+      break; // Hash not in our map
+    } else {
+      map_range_end = MAP_idx;
+      if(USER_idx == MAP - 1) {
+        u8 is_expired;
+        {
+          LOGCTX(" MAP_idx:%x USER_idx:%x ", MAP_idx, MAP - 1 );
+          // Lets remove people if they have been expired for at-least 60 days
+          is_expired = access_user_days_left(USER_idx) <  -60;
+        }
+        if (is_expired) { // This entry is expired lets remove it
+          TRACE("MAP_idx:%x USER_idx:%x Expired, freeing", USER_idx, MAP - 1);
+          USER.debug_is_free = 1;
+          *access_idle_maintenance_prev = USER.next_idx;
+          USER.next_idx = access_users_first_free_idx;
+          access_users_first_free_idx = USER_idx;
+          MAP = (u16)-1;
+        } else {
+          if (map_first_tombstone != (u32)-1) {
+            TRACE("MAP_idx:%x USER_idx:%x We hit a tombstone on the way, lets go ahead and swap this entry(%x) with that one ", map_first_tombstone, MAP - 1, MAP_idx);
+            MAP = (u16)map_TOMB;
+            MAP_idx = map_first_tombstone;
+            MAP = USER_idx + 1;
+          }
+          TRACE("MAP_idx:%x USER_idx:%x Not expired, doing nothing", MAP - 1, MAP_idx);
+          access_idle_maintenance_prev = &USER.next_idx;
+        }
+        break;
+      } else {
+        // TODO, should this really be a warning? or at-least an INFO?
+        TRACE("MAP_idx:%x USER_idx:%x Collision, continuing", MAP_idx, MAP - 1);
+      }
+    }
+  }
+#endif
+
+  // TODO: We could Walk the map range backwards, looking for tombstones... probably not worth it though
+
+}
+
 
 
 // Returns error message if any
@@ -312,83 +355,6 @@ char const * access_user_add(access_HashResult hash, u16 expire_day, u8 extend_o
 
   }
 
-
-
-  // TRACE_HEXBUFFER(hash, 64 / 8, "hash:");
-
-
-  // u32 map_first_tombstone = (u32)-1;
-  // for (u32 MAP_idx = get_hash_i(hash);; MAP_idx = get_hash_mask(MAP_idx + 1) ) {
-  //   if (MAP == map_TOMB) {
-  //     if(  map_first_tombstone == (u32)-1) {
-  //       TRACE("MAP_idx:%x Hit first tombstone ", MAP_idx);
-  //       map_first_tombstone = MAP_idx;
-  //     }
-  //   } else if (MAP == map_EMPTY) {
-  //     // pop off the free list
-  //     u16 USER_idx = access_users_first_free_idx;
-  //     access_users_first_free_idx = USER.next_idx;
-  //     assert(USER.debug_is_free);
-
-  //     if (map_first_tombstone != (u32)-1) {
-  //       TRACE("MAP_idx:%x USER_idx:%x Adding new, replacing tombstone instead of making new slot, got to %x ", map_first_tombstone, USER_idx, MAP_idx);
-  //       MAP_idx = map_first_tombstone;
-  //     } else {
-  //       TRACE("MAP_idx:%x USER_idx:%x Adding new, in new slot", MAP_idx, USER_idx);
-  //     }
-
-  //     if (extend_only) {
-  //       ERROR("extend_only");
-  //       return _msg_extend_only;
-  //     } else {
-  //       USER.debug_is_free = 0;
-  //       USER.next_idx = access_users_first_idx;
-  //       access_users_first_idx = USER_idx;
-
-  //       MAP = USER_idx + 1;
-  //       memcpy(USER.hash, hash, sizeof USER.hash);
-
-  //       USER.expire_day = expire_day;
-
-  //       return 0;
-  //     }
-
-
-  //   } else {
-  //     u16 USER_idx = MAP - 1;
-  //     assert(!USER.debug_is_free);
-  //     if (memcmp(USER.hash, hash, sizeof USER.hash) == 0) {
-
-  //       if (map_first_tombstone != (u32)-1) {
-  //         TRACE("MAP_idx:%x USER_idx:%x We hit a tombstone on the way, lets go ahead and swap this entry(%x) with that one ", map_first_tombstone, USER_idx, MAP_idx);
-  //         MAP = map_TOMB;
-  //         MAP_idx = map_first_tombstone;
-  //         MAP = USER_idx + 1;
-  //       } else {
-  //         TRACE("MAP_idx:%x USER_idx:%x Adding new, in new slot", MAP_idx, USER_idx);
-  //       }
-
-  //       u8 is_admin = (
-  //         USER.expire_day == access_expire_day_magics_Adder       ||
-  //         USER.expire_day == access_expire_day_magics_NewAdder    ||
-  //         USER.expire_day == access_expire_day_magics_Extender    ||
-  //         USER.expire_day == access_expire_day_magics_NewExtender
-  //       );
-
-  //       if (is_admin && !overwrite_admin ) {
-  //         ERROR("overwrite_admin");
-  //         return _msg_overwrite_admin;
-  //       } else {
-  //         TRACE("MAP_idx:%x USER_idx:%x Update existing", MAP_idx, USER_idx);
-  //         USER.expire_day = expire_day;
-  //         return 0;
-  //       }
-  //     } else {
-  //       // TODO, should this really be a warning? or at-least an INFO?
-  //       TRACE("MAP_idx:%x USER_idx:%x Collision, continuing", MAP_idx, USER_idx);
-  //     }
-  //   }
-  //}
 }
 
 access_user_IDX access_user_lookup(access_HashResult hash) {
