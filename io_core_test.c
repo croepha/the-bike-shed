68de519c87f1bce28f0c0e56cb82eddd4a0b3590
Dave Butler <croepha@gmail.com>
2021-09-26 11:49:03 -0753

    io refactor wip

--- io_core_test.c
+++ io_core_test.c
@@ -89,6 +89,45 @@ void io_event_cb(char* name, u32 events, s32 id) { int r;
 }
 
 
+// TODO: remove
+void io_fd_ctl(int flags, int op, enum _io_socket_types type, s32 id, int fd);
+
+
+__attribute__((unused)) static void echo_test_socket(int i, int type, char const * name) { int r;
+    int sv[2] = {-1,-1};
+    r = socketpair(AF_UNIX, SOCK_DGRAM | SOCK_NONBLOCK | SOCK_CLOEXEC, 0, sv); error_check(r);
+    sockets[i] = sv[0];
+
+    pid_t fork_pid = fork();
+    error_check(fork_pid);
+
+    if (!fork_pid) { LOGCTX("\ttest_sort:id:%02d forked", i);
+      r = close(sv[0]); error_check(r);
+
+      u64 now = now_ms();
+      if (start_time > now) {
+        r = usleep((start_time - now) * 1000); error_check(r);
+      }
+
+      int sock = sv[1];
+      long initial_flags = fcntl(sock, F_GETFL, 0); error_check(initial_flags);
+      r = fcntl(sock, F_SETFL, initial_flags & ~O_NONBLOCK); //set nonblock
+      error_check(r);
+
+      r = dprintf(sock, "SEND%02d\n", i); error_check(r);
+      error_check(r);
+      char buf[256]; sock_read_line(sock, buf, sizeof buf);
+      exit(0);
+    }
+
+    r = close(sv[1]); error_check(r);
+
+    io_fd_ctl(EPOLLIN, EPOLL_CTL_ADD, type, i, sockets[i]);
+    INFO("socket id:%02d type:%s:%d", i, name, type);
+    events_pending++;
+}
+
+
 void test_main() {
   io_initialize();
 
